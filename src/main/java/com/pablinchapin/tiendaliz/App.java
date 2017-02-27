a74f5825d75c3cee128d0912e0c605a2898b6b52
pablinchapin <pablinchapin@gmail.com>
2017-02-27 08:12:24 -0600

    Inicio de repositorio

---
+++ src/main/java/com/pablinchapin/tiendaliz/App.java
@@ -0,0 +1,13 @@
+package com.pablinchapin.tiendaliz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
